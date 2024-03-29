b6aef3b9034e768ba5c42764c161ae64ab9a8942
Carlos De la Cruz <me@heyimcarlos.dev>
2024-03-28 23:20:59 -0400

    chore: started the position implementation for the snake

--- src/snake.rs
+++ src/snake.rs
@@ -1,11 +1,34 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, utils::tracing::Instrument};
 
 use crate::board::{Board, TILE_SIZE};
 
 const SNAKE_SPEED: f32 = 30.0;
 
+#[derive(Resource, Debug)]
+pub struct SnakeBody {
+    segments: Vec<Position>,
+}
+
+impl Default for SnakeBody {
+    fn default() -> Self {
+        Self {
+            segments: Vec::from([
+                Position { x: 3, y: 1 },
+                Position { x: 2, y: 1 },
+                Position { x: 1, y: 1 },
+            ]),
+        }
+    }
+}
+
 #[derive(Component, Debug)]
 pub struct Snake;
+
+#[derive(Debug, Component)]
+pub struct Position {
+    x: u8,
+    y: u8,
+}
 
 #[derive(Component, Debug)]
 pub struct SnakeDirection {
@@ -21,8 +44,8 @@ pub struct MovementTimer {
 impl Default for SnakeDirection {
     fn default() -> Self {
         SnakeDirection {
-            value: Direction::Up,
-            next_value: Direction::Up,
+            value: Direction::Right,
+            next_value: Direction::Right,
         }
     }
 }
@@ -32,8 +55,9 @@ pub struct SnakePlugin;
 impl Plugin for SnakePlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(MovementTimer {
-            timer: Timer::from_seconds(0.4, TimerMode::Repeating),
+            timer: Timer::from_seconds(2., TimerMode::Repeating),
         })
+        .init_resource::<SnakeBody>()
         .add_systems(PostStartup, spawn_snake)
         .add_systems(Update, snake_movement_controls);
     }
@@ -48,17 +72,19 @@ pub enum Direction {
     Right,
 }
 
-fn spawn_snake(mut commands: Commands, board: Res<Board>) {
+fn spawn_snake(mut commands: Commands, board: Res<Board>, snake: Res<SnakeBody>) {
+    // get snake head
+    let head = snake.segments.first().unwrap();
     commands.spawn((
         SpriteBundle {
             transform: Transform::from_xyz(
                 // @todo: randomize the spawn of the snake
-                board.position_translate(0),
-                board.position_translate(19),
+                board.position_translate(head.x.into()),
+                board.position_translate(head.y.into()),
                 10.0,
             ),
             sprite: Sprite {
-                color: Color::GRAY,
+                color: Color::RED,
                 custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
                 ..default()
             },
@@ -67,6 +93,27 @@ fn spawn_snake(mut commands: Commands, board: Res<Board>) {
         Snake,
         SnakeDirection::default(),
     ));
+
+    let tail = &snake.segments[1..];
+    for segment in tail {
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(
+                    // @todo: randomize the spawn of the snake
+                    board.position_translate(segment.x.into()),
+                    board.position_translate(segment.y.into()),
+                    10.0,
+                ),
+                sprite: Sprite {
+                    color: Color::GRAY,
+                    custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
+                    ..default()
+                },
+                ..default()
+            },
+            Snake,
+        ));
+    }
 }
 
 fn snake_movement_controls(
@@ -74,42 +121,93 @@ fn snake_movement_controls(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
     board: Res<Board>,
+    mut snake: ResMut<SnakeBody>,
     mut movement_timer: ResMut<MovementTimer>,
 ) {
-    let Ok((mut transform, mut direction)) = query.get_single_mut() else {
+    // for (mut transform, direction) in query.iter_mut() {
+    //     println!("Snake position: {:?}", transform.translation);
+    // }
+    let Ok((mut transform, mut snake_direction)) = query.get_single_mut() else {
         return;
     };
-    dbg!(
-        transform.translation.x,
-        transform.translation.y,
-        direction.value,
-        direction.next_value
-    );
-
-    if direction.value != direction.next_value {
-        direction.value = direction.next_value;
+    println!("Snake's head direction: {:?}", snake_direction);
+    // dbg!(
+    //     transform.translation.x,
+    //     transform.translation.y,
+    //     direction.value,
+    //     direction.next_value
+    // );
+    //
+    if snake_direction.value != snake_direction.next_value {
+        snake_direction.value = snake_direction.next_value;
     }
-
-    // @info: this is a decent strategy. But what if we used positoin instead of direction?
-    if keyboard_input.pressed(KeyCode::ArrowUp) && direction.value != Direction::Down {
-        direction.next_value = Direction::Up;
-    } else if keyboard_input.pressed(KeyCode::ArrowDown) && direction.value != Direction::Up {
-        direction.next_value = Direction::Down;
-    } else if keyboard_input.pressed(KeyCode::ArrowLeft) && direction.value != Direction::Right {
-        direction.next_value = Direction::Left;
-    } else if keyboard_input.pressed(KeyCode::ArrowRight) && direction.value != Direction::Left {
-        direction.next_value = Direction::Right;
+    //
+    // // @info: this is a decent strategy. But what if we used positoin instead of direction?
+    if keyboard_input.pressed(KeyCode::ArrowUp) && snake_direction.value != Direction::Down {
+        snake_direction.next_value = Direction::Up;
+    } else if keyboard_input.pressed(KeyCode::ArrowDown) && snake_direction.value != Direction::Up {
+        snake_direction.next_value = Direction::Down;
+    } else if keyboard_input.pressed(KeyCode::ArrowLeft)
+        && snake_direction.value != Direction::Right
+    {
+        snake_direction.next_value = Direction::Left;
+    } else if keyboard_input.pressed(KeyCode::ArrowRight)
+        && snake_direction.value != Direction::Left
+    {
+        snake_direction.next_value = Direction::Right;
     }
-
+    //
     if !movement_timer.timer.tick(time.delta()).just_finished() {
         return;
     }
-    dbg!(movement_timer.timer.elapsed_secs());
 
-    match direction.value {
-        Direction::Up => transform.translation.y += TILE_SIZE,
-        Direction::Down => transform.translation.y -= TILE_SIZE,
-        Direction::Left => transform.translation.x -= TILE_SIZE,
-        Direction::Right => transform.translation.x += TILE_SIZE,
-    }
+    // @info: we have to create a new snake segnment and spawn it as the new head. then we have to
+    // remove the last segment of the snake. This is how we simulate the snake moving.
+
+    // move head
+    let new_segment = snake.segments.first().clone();
+
+    // match snake_direction.value {
+    //     Direction::Up => {
+    //         let mut head = snake.segments.first_mut().unwrap();
+    //         head.y += 1;
+    //         let mut tail = snake.segments.iter_mut().skip(1);
+    //         for segment in tail {
+    //             segment.y += 1;
+    //         }
+    //     }
+    //     Direction::Down => {
+    //         let mut head = snake.segments.first_mut().unwrap();
+    //         head.y -= 1;
+    //         let mut tail = snake.segments.iter_mut().skip(1);
+    //         for segment in tail {
+    //             segment.y -= 1;
+    //         }
+    //     }
+    //     Direction::Left => {
+    //         let mut head = snake.segments.first_mut().unwrap();
+    //         head.x -= 1;
+    //         let mut tail = snake.segments.iter_mut().skip(1);
+    //         for segment in tail {
+    //             segment.x -= 1;
+    //         }
+    //     }
+    //     Direction::Right => {
+    //         let mut head = snake.segments.first_mut().unwrap();
+    //         head.x += 1;
+    //         let mut tail = snake.segments.iter_mut().skip(1);
+    //         for segment in tail {
+    //             segment.x += 1;
+    //         }
+    //     }
+    // }
+
+    // dbg!(movement_timer.timer.elapsed_secs());
+    //
+    // match snake_direction.value {
+    //     Direction::Up => transform.translation.y += TILE_SIZE,
+    //     Direction::Down => transform.translation.y -= TILE_SIZE,
+    //     Direction::Left => transform.translation.x -= TILE_SIZE,
+    //     Direction::Right => transform.translation.x += TILE_SIZE,
+    // }
 }
