64a38c8c0cf6adbc638c6992c481a4095b5ed402
Carlos De la Cruz <me@heyimcarlos.dev>
2024-04-05 10:46:20 -0400

    chore: write detect direction util fn

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
 
-use crate::snake::Position;
+use crate::snake::{Direction, Position};
 
 pub fn snake_starting_position(board_size: i32) -> Vec<Position> {
     // let mut rng = rand::thread_rng();
@@ -35,4 +35,18 @@ pub fn food_position(board_size: i32) -> Position {
 
 pub fn calc_sprite_index(row: usize, column: usize, columns_per_row: usize) -> usize {
     row * columns_per_row + column
+}
+
+pub fn detect_direction(origin: &Position, next: &Position) -> Direction {
+    if next.y > origin.y {
+        Direction::Up
+    } else if next.y < origin.y {
+        Direction::Down
+    } else if next.x > origin.y {
+        Direction::Right
+    } else if next.x < origin.x {
+        Direction::Left
+    } else {
+        panic!("Invalid direction")
+    }
 }
